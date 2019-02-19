364accf4a649bccdc385c0b4a66f6d04412da555
zhoufangan <zhoufangan@zafh.com.cn>
2019-02-19 17:46:08 +0800

    测试项目

---
+++ future-02-testingwork/src/main/java/com/zhoufa/Future02TestingWorkApplication.java
@@ -0,0 +1,14 @@
+package com.zhoufa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhoufangan !
+ */
+@SpringBootApplication
+public class Future02TestingWorkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Future02TestingWorkApplication.class, args);
+    }
+}
---
+++ future-02-testingwork/src/test/java/com/zhoufa/Future02TestingWorkApplicationTest.java
@@ -0,0 +1,21 @@
+package com.zhoufa;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Unit test for simple Future02TestingWorkApplication.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Future02TestingWorkApplicationTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
