12c7191c0f829a552fab9d7e3e63f58ccd67a628
zhoufangan <12345678>
2019-10-15 17:19:04 +0800

    redis module相关初始化

---
+++ future-04-redis/src/main/java/com/zhoufa/Future04RedisApplication.java
@@ -0,0 +1,15 @@
+package com.zhoufa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhoufangan !
+ */
+@SpringBootApplication
+public class Future04RedisApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Future04RedisApplication.class, args);
+    }
+}
---
+++ future-04-redis/src/test/java/com/zhoufa/Future04RedisApplicationTest.java
@@ -0,0 +1,19 @@
+package com.zhoufa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Unit test for simple Future04RedisApplication.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Future04RedisApplicationTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+
+    }
+}
