372379bc0a01b605491c16b94ef11fa20bfe64f7
zhoufangan <12345678>
2019-10-22 15:15:35 +0800

    rocketmq Linux配置完成

--- future-04-redis/src/main/java/com/zhoufa/Future04RedisApplication.java
+++ future-04-redis/src/main/java/com/zhoufa/Future04RedisApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Future04RedisApplication {
+
     public static void main(String[] args) {
 
         SpringApplication.run(Future04RedisApplication.class, args);
