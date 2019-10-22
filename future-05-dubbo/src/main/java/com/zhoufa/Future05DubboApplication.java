202e028de1746e0ae0254ef5927f22f86e376633
zhoufangan <1529725362@qq.com>
2019-10-22 22:49:19 +0800

    创建dubbo，zk工程

---
+++ future-05-dubbo/src/main/java/com/zhoufa/Future05DubboApplication.java
@@ -0,0 +1,15 @@
+package com.zhoufa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhoufangan !
+ */
+@SpringBootApplication
+public class Future05DubboApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Future05DubboApplication.class, args);
+    }
+}
