d38b4c090522f0a7f0fbb5d4c76b27ae3f95db17
zhoufangan <12345678>
2019-11-19 16:16:46 +0800

    zk 相关demo

---
+++ future-06-zookeeper/src/main/java/com/zhoufa/zookeeper/Demo_03_Curator.java
@@ -0,0 +1,11 @@
+package com.zhoufa.zookeeper;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zhoufangan. Founded on 2019/10/29 11:27.
+ * @version V1.0
+ */
+@Component
+public class Demo_03_Curator {
+}
---
+++ future-06-zookeeper/src/main/java/com/zhoufa/zookeeper/Demo_04_ConfigManagement.java
@@ -0,0 +1,11 @@
+package com.zhoufa.zookeeper;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zhoufangan. Founded on 2019/10/29 11:28.
+ * @version V1.0
+ */
+@Component
+public class Demo_04_ConfigManagement {
+}
